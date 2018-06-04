c4062c8c054930d6453043ba50859e3a0c040a9c
danial-abadi <danial.abadi.ghadim@gmail.com>
2018-06-04 23:36:59 +0430

    initial commit

---
+++ main/Main.java
@@ -0,0 +1,22 @@
+package main;
+
+/**
+ * this is a sample Hello world class
+ * 
+ * @author daniel
+ *
+ */
+public class Main
+{
+
+	/**
+	 * this is main method of program. program start from here
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		System.out.println("Hello GitHub :)");
+	}
+
+}
